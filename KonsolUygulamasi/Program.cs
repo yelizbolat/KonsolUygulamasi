ba3566dbc1118d78f4e3ae978f64704011cee2f6
bolat <bolat@YELIZ>
2023-10-31 10:25:54 +0300

    Proje dosyası ekle.

---
+++ KonsolUygulamasi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("herkese salam");
+            Console.WriteLine("33 yasim var");
+            Console.WriteLine("bekarimdir.");
+        }
+    }
+}
